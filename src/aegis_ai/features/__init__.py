5d5043425a44685060c9744f1c05b2819635561e
Kamil Dudka <kdudka@redhat.com>
2026-09-17 13:56:01 +0200

    config: double the default value for `AEGIS_LLM_INPUT_TOKENS_WARN_THR`
    The `gemini-3.8-flash` model seems to consume more tokens than
    `gemini-2.5-flash` in the same scenarios, which caused too many
    warnings to be printed in the log even when everything was working
    as expected.
    
    Related: https://redhat.atlassian.net/browse/AEGIS-506

--- src/aegis_ai/features/__init__.py
+++ src/aegis_ai/features/__init__.py
@@ -104,7 +104,7 @@ llm_prompt_timeout = get_settings().default_llm_prompt_timeout
 llm_sem = asyncio.Semaphore(get_settings().llm_max_jobs)
 
 # The threshold for LLM input tokens to log a warning
-llm_input_tokens_warn_thr = get_env_int("AEGIS_LLM_INPUT_TOKENS_WARN_THR", 65536)
+llm_input_tokens_warn_thr = get_env_int("AEGIS_LLM_INPUT_TOKENS_WARN_THR", 131072)
 
 
 # initial delay in seconds after getting HTTP 503 status code from LLM (doubled on each attempt)
